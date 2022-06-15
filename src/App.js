6dad3b97ca22e98053585e4a834ea15acb997fd3
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-06-15 23:30:11 +0400

    create static navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+      </div>
+
+      <main>Content</main>
+    </BrowserRouter>
   );
 }
 
