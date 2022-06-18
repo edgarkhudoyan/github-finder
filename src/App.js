9e5074f9209f87b6f16d451e629ecc3b5ff285cd
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-06-18 23:27:57 +0400

    add alert provider

--- src/App.js
+++ src/App.js
@@ -5,26 +5,29 @@ import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>
-      <BrowserRouter>
-        <div className="flex flex-col justify-between">
-          <Navbar />
-        </div>
+      <AlertProvider>
+        <BrowserRouter>
+          <div className="flex flex-col justify-between">
+            <Navbar />
+          </div>
 
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
 
-        <Footer />
-      </BrowserRouter>
+          <Footer />
+        </BrowserRouter>
+      </AlertProvider>
     </GithubProvider>
   )
 }
