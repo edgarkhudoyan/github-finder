5eee6412d218f33537c6db0ba707fcaf45575e3a
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-06-18 23:27:18 +0400

    fix users not showing up

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -32,6 +32,11 @@ export const GithubProvider = ({ children }) => {
     })
   }
 
+  // Clear users from state
+  const clearUsers = () => {
+    dispatch({ type: 'CLEAR_USERS' })
+  }
+
   // Set loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
@@ -41,6 +46,7 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         searchUsers,
+        clearUsers,
       }}
     >
       {children}
